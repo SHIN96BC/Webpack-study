1845dac334b2e222d188ba3948f138960795ba88
SHIN96BC <mousecjf@gmail.com>
2023-06-15 22:06:55 +0900

    두번째 과제

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,32 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
+    assetModuleFilename: '[hash][ext][query]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      // url-loader 사용시
+      { 
+        test: /\.(jpg|png)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024 // 8kb
+          }
+        }
+      }
+      // file-loader 사용시
+      // {
+      //   test: /\.(jpg|png)$/,
+      //   type: 'asset/resource'
+      // }
+    ]
   }
 }
